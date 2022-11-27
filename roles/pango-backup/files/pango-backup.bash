56d34449bea8538a26d6538f7b1cf2ec257d8b1e
Mathias Palmersheim <mathias@shiftsystems.net>
2022-11-26 21:55:12 -0600

    fixed #1 diskspec includes filepath

--- roles/pango-backup/files/pango-backup.bash
+++ roles/pango-backup/files/pango-backup.bash
@@ -8,17 +8,26 @@ for i in $(virsh list --name); do
 done
 
 echo "backup configs"
-borg create --progress --compression zstd,6 $BORG_URI::vm-configs /tmp/vmconfigs
+BACKUP_TIME=$(date +%Y%m%d%H%M%S)
+borg create --progress --compression zstd,6 $BORG_URI::vm-configs-${BACKUP_TIME} /tmp/vmconfigs
+
+echo "making list of volumes"
+rm -f /tmp/vollist.txt
+for i in $(virsh pool-list --name); do
+  echo "$(virsh vol-list $i | awk '{print $2}' | grep qcow2)" >> /tmp/vollist.txt
+  echo "\n" >> /tmp/vollist.txt
+done
 
 echo "creating snapshots"
 for i in $(virsh list --name); do
-    qcow_path=$(virsh domblklist $i | grep qcow2 | awk '{print $2}')
     dev_name=$(virsh domblklist $i | grep qcow2 | awk '{print $1}')
-    backup_path=$(echo "$qcow_path" | sed "s/.qcow2/.pangobackup.qcow2/g")
-    echo "now creating snapshot for ${i} located at: $qcow_path"
-    virsh snapshot-create-as --domain ${i} --name pangobackup.qcow2 --no-metadata --atomic --quiesce --disk-only --diskspec ${dev_name},snapshot=external
+    qcow_name=$(virsh domblklist $i | grep qcow2 | awk '{print $2}')
+    qcow_path=$(grep "${qcow_name}" /tmp/vollist.txt | tail -n 1)
+    backup_path=$(echo "${qcow_path}" | sed "s/.qcow2/.pangobackup.qcow2/g")
+    echo "now creating snapshot for ${i} volume name: ${qcow_name} located at: ${qcow_path}"
+    virsh snapshot-create-as --domain ${i} --no-metadata --atomic --quiesce --disk-only --diskspec ${dev_name},snapshot=external,file=${backup_path}
     echo "now backing up ${i}"
-    borg create --progress --compression zstd,6 $BORG_URI::${i} ${qcow_path}
+    borg create --progress --compression zstd,6 $BORG_URI::${i}-${BACKUP_TIME} ${qcow_path}
     echo "${i} has been backed up successfully"
     echo "now combining snapshot for ${i}"
     virsh blockcommit ${i} ${dev_name} --active --pivot
@@ -27,3 +36,4 @@ for i in $(virsh list --name); do
     rm -f $backup_path
     echo "$backup_path has been removed";
 done
+
